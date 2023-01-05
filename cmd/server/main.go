58c6384b9946dcdf5980e61c39bbf05f42cebcbc
untalDouglas <galindo@untaldouglas.info>
2023-01-05 16:53:06 -0600

    Conexión a DB Postgres configurada pero con error de puerto

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/untaldouglas/go-rest-api/internal/db"
+)
 
 // Run - Responsable de inicializar el module
 // tareas como iniciar conexión a DB van acá
 func Run() error {
 	fmt.Println("Iniciando la aplicación ...")
+
+	db, err := db.NewDatabase()
+	if err != nil {
+		fmt.Println("Error al conectarse a la base de datos")
+		return err
+	}
+
+	// Eliminar posteriormente
+	if err := db.Ping(context.Background()); err != nil {
+		return err
+	}
+
+	fmt.Println("*** Succesfully connected and pinged database !!")
 	return nil
 }
 
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/untaldouglas/go-rest-api
 
 go 1.19
+
+require (
+	github.com/jmoiron/sqlx v1.3.5 // indirect
+	github.com/lib/pq v1.10.7 // indirect
+)
