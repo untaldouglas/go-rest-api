0dcc5b6bc10c828e3714cd40a545b537814aab5d
untalDouglas <galindo@untaldouglas.info>
2023-01-10 15:35:22 -0600

    Finalización controlada con shutdown

--- internal/transport/http/handler.go
+++ internal/transport/http/handler.go
@@ -1,8 +1,13 @@
 package http
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -26,7 +31,7 @@ func NewHandler(service OpinionService) *Handler {
 		Addr:    "0.0.0.0:8080",
 		Handler: h.Router,
 	}
- 
+
 	return h
 }
 
@@ -37,8 +42,20 @@ func (h *Handler) mapRoutes() {
 }
 
 func (h *Handler) Serve() error {
-	if err := h.Server.ListenAndServe(); err != nil {
-		return err
-	}
+	go func() {
+		if err := h.Server.ListenAndServe(); err != nil {
+			log.Println(err.Error())
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+	h.Server.Shutdown(ctx)
+
+	log.Println("shut down gracefully")
 	return nil
 }
